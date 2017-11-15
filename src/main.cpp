1db30aa77ffa0ca99ee12bc7f6feb97a63f2cb90
hoshi524 <hoshi524@gmail.com>
2017-11-15 12:12:04 +0900

    update

--- src/main.cpp
+++ src/main.cpp
@@ -19,7 +19,7 @@ int X[MAX_KVV];
 int* x = X + MAX_KV;
 int BEST[MAX_KVV];
 int* best = BEST + MAX_KV;
-bool used[MAX_V];
+int vertexes[MAX_V];
 
 inline int _abs(int v) {
   int mask = v >> 31;
@@ -52,36 +52,38 @@ int main() {
   }
   {  // solve
     int score = 0;
-    memset(used, false, sizeof(used));
-    for (int i = 0; i < MAX_KVV; ++i) {
-      X[i] = V;
-    }
-    int s = 0;
-    for (int i = 0; i < V; ++i) {
-      int n = 0, v = -1;
-      for (int j = 0; j < V; ++j) {
-        if (used[j]) continue;
-        int t = 0;
-        auto add = [&](int i, int j) {
-          if (connect(i, j)) t += w[x[i]][x[j]];
-        };
-        x[i] = j;
-        add(i, i - 1);
-        add(i, i - R);
-        add(i, i - R - 1);
-        add(i, i - R + 1);
-        if (v < t) {
-          v = t;
-          n = j;
+    random_device seed_gen;
+    mt19937 engine(seed_gen());
+    for (int time = 0; time < 1000; ++time) {
+      for (int i = 0; i < MAX_KVV; ++i) X[i] = V;
+      for (int i = 0; i < V; ++i) vertexes[i] = i;
+      shuffle(vertexes, vertexes + V, engine);
+      int s = 0;
+      for (int i = 0; i < V; ++i) {
+        int n = 0, v = -1;
+        for (int j = 0; j < V - i; ++j) {
+          int t = 0;
+          auto add = [&](int i, int j) {
+            if (connect(i, j)) t += w[x[i]][x[j]];
+          };
+          x[i] = vertexes[j];
+          add(i, i - 1);
+          add(i, i - R);
+          add(i, i - R - 1);
+          add(i, i - R + 1);
+          if (v < t) {
+            v = t;
+            n = j;
+          }
         }
+        x[i] = vertexes[n];
+        vertexes[n] = vertexes[V - i - 1];
+        s += v;
       }
-      used[n] = true;
-      x[i] = n;
-      s += v;
-    }
-    if (score < s) {
-      score = s;
-      memcpy(BEST, X, sizeof(X));
+      if (score < s) {
+        score = s;
+        memcpy(BEST, X, sizeof(X));
+      }
     }
     fprintf(stderr, "score = %d\n", score);
   }
