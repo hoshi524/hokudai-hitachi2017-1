490f508a5b3039b221840a0a3266095341f891f6
hoshi524 <hoshi524@gmail.com>
2017-11-15 19:12:17 +0900

    update

--- src/annealing.cpp
+++ src/annealing.cpp
@@ -1,7 +1,61 @@
 #include <bits/stdc++.h>
+#include <sys/time.h>
 using namespace std;
 
-constexpr int ROW = 1 << 6;
+class Timer {
+ public:
+  void restart();
+  double getElapsed();
+
+  Timer();
+
+ private:
+  static double rdtsc_per_sec_inv;
+
+  double getTimeOfDay();
+  unsigned long long int getCycle();
+
+  double start_time;
+  unsigned long long int start_clock;
+};
+double Timer::rdtsc_per_sec_inv = -1;
+
+inline double Timer::getElapsed() {
+  if (rdtsc_per_sec_inv != -1)
+    return (double)(getCycle() - start_clock) * rdtsc_per_sec_inv;
+
+  const double RDTSC_MEASUREMENT_INTERVAL = 0.1;
+  double res = getTimeOfDay() - start_time;
+  if (res <= RDTSC_MEASUREMENT_INTERVAL) return res;
+
+  rdtsc_per_sec_inv = 1.0 / (getCycle() - start_clock);
+  rdtsc_per_sec_inv *= getTimeOfDay() - start_time;
+  return getElapsed();
+}
+
+inline void Timer::restart() {
+  start_time = getTimeOfDay();
+  start_clock = getCycle();
+}
+
+Timer::Timer() { restart(); }
+
+inline double Timer::getTimeOfDay() {
+  timeval tv;
+  gettimeofday(&tv, 0);
+  return tv.tv_sec + tv.tv_usec * 0.000001;
+}
+
+inline unsigned long long int Timer::getCycle() {
+  unsigned int low, high;
+  __asm__ volatile("rdtsc" : "=a"(low), "=d"(high));
+  return ((unsigned long long int)low) | ((unsigned long long int)high << 32);
+}
+
+Timer timer;
+
+constexpr double TIME_LIMIT = 9.5;
+constexpr int ROW = 1 << 5;
 constexpr int MAX_V = ROW * ROW;
 
 unsigned get_random() {
@@ -12,13 +66,11 @@ unsigned get_random() {
 double get_random_double() { return (double)get_random() / UINT_MAX; }
 
 int V, E, KV, KE, KR;
-int s[MAX_V];
-int w[MAX_V][MAX_V];
+uint8_t w[MAX_V][MAX_V];
 int x[MAX_V];
 
 int main() {
   {  // input
-    memset(s, 0, sizeof(s));
     memset(w, 0, sizeof(w));
     scanf("%d%d\n", &V, &E);
     int u, v, t;
@@ -56,45 +108,33 @@ int main() {
       v += w[x[p]][x[p + ROW + 1]];
       return v;
     };
-    constexpr int max_time = 0x4000000;
-    for (int time = 0; time < max_time; ++time) {
-      int p1 = (get_random() % r + 1) * ROW + (get_random() % r + 1);
-      int p2 = (get_random() % r + 1) * ROW + (get_random() % r + 1);
-      if (p1 == p2) continue;
-      int pv = value(p1) + value(p2);
-      int t = x[p1];
-      x[p1] = x[p2];
-      x[p2] = t;
-      int nv = value(p1) + value(p2);
-      if (nv >= pv || (pv - nv) < -5 * log(get_random_double()) *
-                                      (max_time - time) / max_time) {
-      } else {
-        t = x[p1];
-        x[p1] = x[p2];
-        x[p2] = t;
-      }
+    constexpr int max_time = 0x8000000;
+    constexpr int LOG_SIZE = 1 << 12;
+    static double log_[LOG_SIZE];
+    for (int i = 0; i < LOG_SIZE; ++i) {
+      log_[i] = -5 * log((i + 0.5) / LOG_SIZE) / TIME_LIMIT;
     }
-
-    int minRow = ROW;
-    int minCol = ROW;
-    for (int i = 0; i < MAX_V; ++i) {
-      if (x[i] < V) {
-        int r = i & (ROW - 1);
-        int c = i >> 6;
-        if (minRow > r) minRow = r;
-        if (minCol > c) minCol = c;
-      }
-    }
-    for (int i = 0; i < MAX_V; ++i) {
-      if (x[i] < V) {
-        x[i - minCol * ROW - minRow] = x[i];
-        x[i] = V;
+    while (true) {
+      double elapsed = timer.getElapsed();
+      if (elapsed > TIME_LIMIT) break;
+      for (int time = 0; time < 0x10000; ++time) {
+        int p1 = (get_random() % r + 1) * ROW + (get_random() % r + 1);
+        int p2 = (get_random() % r + 1) * ROW + (get_random() % r + 1);
+        if (p1 == p2) continue;
+        int pv = value(p1) + value(p2);
+        swap(x[p1], x[p2]);
+        int nv = value(p1) + value(p2);
+        if (nv >= pv || (pv - nv) < log_[get_random() & (LOG_SIZE - 1)] *
+                                        (TIME_LIMIT - elapsed)) {
+        } else {
+          swap(x[p1], x[p2]);
+        }
       }
     }
   }
   {  // output
     for (int i = 0; i < MAX_V; ++i) {
-      if (x[i] < V) printf("%d %d\n", x[i] + 1, i / ROW * KR + i % ROW + 1);
+      if (x[i] < V) printf("%d %d\n", x[i] + 1, (i / ROW - 1) * KR + i % ROW);
     }
   }
 }
--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@ set -e
 cd `dirname $0`
 
 g++ -o ./out/score_evaluator.out ./toolkit/scripts/score_evaluator.cpp
-g++ --std=c++0x -W -Wall -Wno-sign-compare -O2 -s -pipe -mmmx -msse -msse2 -msse3 -o out/solve.out src/main.cpp
+g++ --std=c++0x -W -Wall -Wno-sign-compare -O2 -s -pipe -mmmx -msse -msse2 -msse3 -o out/solve.out src/annealing.cpp
 
 for i in `seq 1 100`
 do
