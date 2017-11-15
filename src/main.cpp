ae9017c4d88cc8ca0a0ca5b8ca3c8547bb916f67
hoshi524 <hoshi524@gmail.com>
2017-11-16 08:50:02 +0900

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,77 @@
 #include <bits/stdc++.h>
+#include <sys/time.h>
 using namespace std;
 
-constexpr int ROW = 1 << 6;
+class Timer {
+ public:
+  void restart();
+  double getElapsed();
+
+  Timer();
+
+ private:
+  static double rdtsc_per_sec_inv;
+
+  double getTimeOfDay();
+  unsigned long long int getCycle();
+
+  double start_time;
+  unsigned long long int start_clock;
+};
+double Timer::rdtsc_per_sec_inv = -1;
+
+inline double Timer::getElapsed() {
+  if (rdtsc_per_sec_inv != -1)
+    return (double)(getCycle() - start_clock) * rdtsc_per_sec_inv;
+
+  const double RDTSC_MEASUREMENT_INTERVAL = 0.1;
+  double res = getTimeOfDay() - start_time;
+  if (res <= RDTSC_MEASUREMENT_INTERVAL) return res;
+
+  rdtsc_per_sec_inv = 1.0 / (getCycle() - start_clock);
+  rdtsc_per_sec_inv *= getTimeOfDay() - start_time;
+  return getElapsed();
+}
+
+inline void Timer::restart() {
+  start_time = getTimeOfDay();
+  start_clock = getCycle();
+}
+
+Timer::Timer() { restart(); }
+
+inline double Timer::getTimeOfDay() {
+  timeval tv;
+  gettimeofday(&tv, 0);
+  return tv.tv_sec + tv.tv_usec * 0.000001;
+}
+
+inline unsigned long long int Timer::getCycle() {
+  unsigned int low, high;
+  __asm__ volatile("rdtsc" : "=a"(low), "=d"(high));
+  return ((unsigned long long int)low) | ((unsigned long long int)high << 32);
+}
+
+Timer timer;
+
+constexpr double TIME_LIMIT = 2.8;
+constexpr int ROW = 1 << 5;
 constexpr int MAX_V = ROW * ROW;
 
-int V, E, KV, KE, KR;
-int s[MAX_V];
-int w[MAX_V][MAX_V];
-int x[MAX_V];
-int best[MAX_V];
-int vertexes[MAX_V];
-int positions[MAX_V];
-bool ok[MAX_V];
-
-inline int _abs(int v) {
-  int mask = v >> 31;
-  return (v + mask) ^ mask;
+inline unsigned get_random() {
+  static unsigned y = 2463534242;
+  return y ^= (y ^= (y ^= y << 13) >> 17) << 5;
 }
+
+inline double get_random_double() { return (double)get_random() / UINT_MAX; }
+
+int V, E, KV, KE, KR;
+uint8_t w[500][500];
+uint16_t x[MAX_V];
 
 int main() {
   {  // input
-    memset(s, 0, sizeof(s));
     memset(w, 0, sizeof(w));
-    memset(ok, false, sizeof(ok));
     scanf("%d%d\n", &V, &E);
     int u, v, t;
     for (int i = 0; i < E; ++i) {
@@ -38,82 +87,51 @@ int main() {
     KR = sqrt(KV);
   }
   {  // solve
-    int bestScore = 0;
-    int CENTOR = (KR / 2 + 1) * ROW + (KR / 2 + 1);
-    random_device seed_gen;
-    mt19937 engine(seed_gen());
-    for (int time = 0; time < 100; ++time) {
-      for (int i = 1; i <= KR; ++i)
-        for (int j = 1; j <= KR; ++j) ok[i * ROW + j] = true;
-      for (int i = 0; i < MAX_V; ++i) x[i] = V;
-      for (int i = 0; i < V; ++i) vertexes[i] = i;
-      shuffle(vertexes, vertexes + V, engine);
-      int score = 0, cd = 0, ps = 0;
-      auto add = [&](int p) {
-        if (ok[p]) {
-          ok[p] = false;
-          positions[ps++] = p;
-        }
-      };
-      add(CENTOR);
-      for (int i = 0; i < V; ++i) {
-        int vi = 0, pi = 0, value = -1;
-        for (int j = 0; j < V - i; ++j) {
-          int v = vertexes[j];
-          for (int k = 0; k < ps; ++k) {
-            int p = positions[k];
-            int t = 0;
-            t += w[v][x[p - ROW - 1]];
-            t += w[v][x[p - ROW]];
-            t += w[v][x[p - ROW + 1]];
-            t += w[v][x[p - 1]];
-            t += w[v][x[p + 1]];
-            t += w[v][x[p + ROW - 1]];
-            t += w[v][x[p + ROW]];
-            t += w[v][x[p + ROW + 1]];
-            if (value < t) {
-              value = t;
-              vi = j;
-              pi = k;
-            }
-          }
-        }
-        add(positions[pi] + 1);
-        add(positions[pi] - 1);
-        add(positions[pi] + ROW);
-        add(positions[pi] - ROW);
-        x[positions[pi]] = vertexes[vi];
-        vertexes[vi] = vertexes[V - i - 1];
-        positions[pi] = positions[--ps];
-        score += value;
-      }
-      if (bestScore < score) {
-        bestScore = score;
-        memcpy(best, x, sizeof(x));
+    int r = sqrt(V);
+    if (r * r < V) ++r;
+    int n = 0;
+    for (int i = 0; i < MAX_V; ++i) x[i] = V;
+    for (int i = 1; i <= r && n < V; ++i) {
+      for (int j = 1; j <= r && n < V; ++j) {
+        x[i * ROW + j] = n++;
       }
     }
-    int minRow = ROW;
-    int minCol = ROW;
-    for (int i = 0; i < MAX_V; ++i) {
-      if (best[i] < V) {
-        int r = i & (ROW - 1);
-        int c = i >> 6;
-        if (minRow > r) minRow = r;
-        if (minCol > c) minCol = c;
+    auto value = [](int p) {
+      int v = 0;
+      v += w[x[p]][x[p - ROW - 1]];
+      v += w[x[p]][x[p - ROW]];
+      v += w[x[p]][x[p - ROW + 1]];
+      v += w[x[p]][x[p - 1]];
+      v += w[x[p]][x[p + 1]];
+      v += w[x[p]][x[p + ROW - 1]];
+      v += w[x[p]][x[p + ROW]];
+      v += w[x[p]][x[p + ROW + 1]];
+      return v;
+    };
+    constexpr int LOG_SIZE = 1 << 12;
+    static double log_d[LOG_SIZE];
+    static uint8_t log_[LOG_SIZE];
+    for (int i = 0; i < LOG_SIZE; ++i) {
+      log_d[i] = -5 * log((i + 0.5) / LOG_SIZE) / TIME_LIMIT;
+    }
+    while (true) {
+      double time = TIME_LIMIT - timer.getElapsed();
+      if (time < 0) break;
+      for (int i = 0; i < LOG_SIZE; ++i) log_[i] = log_d[i] * time;
+      for (int t = 0; t < 0x20000; ++t) {
+        int p1 = ((get_random() % r + 1) << 5) | (get_random() % r + 1);
+        int p2 = ((get_random() % r + 1) << 5) | (get_random() % r + 1);
+        if (x[p1] == x[p2]) continue;
+        int pv = value(p1) + value(p2);
+        swap(x[p1], x[p2]);
+        int nv = value(p1) + value(p2);
+        if ((pv - nv) > log_[get_random() & (LOG_SIZE - 1)]) swap(x[p1], x[p2]);
       }
     }
-    for (int i = 0; i < MAX_V; ++i) {
-      if (best[i] < V) {
-        best[i - minCol * ROW - minRow] = best[i];
-        best[i] = V;
-      }
-    }
-    fprintf(stderr, "score = %d\n", bestScore);
   }
   {  // output
     for (int i = 0; i < MAX_V; ++i) {
-      if (best[i] < V)
-        printf("%d %d\n", best[i] + 1, i / ROW * KR + i % ROW + 1);
+      if (x[i] < V) printf("%d %d\n", x[i] + 1, (i / ROW - 1) * KR + i % ROW);
     }
   }
 }
