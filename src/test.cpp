a5ca7fd7954fa8d66f4aad353e57beccb9d3cd29
hoshi524 <hoshi524@gmail.com>
2017-11-22 16:00:06 +0900

    update

--- src/test.cpp
+++ src/test.cpp
@@ -123,14 +123,12 @@ int main() {
     bool ok[MAX_KV];
     memset(ok, false, sizeof(ok));
     vector<int16_t> vertexes(MAX_V);
+    vertexes.clear();
     vector<int16_t> positions(MAX_KV);
-    mt19937 engine(get_random());
     for (int time = 0; time < 100; ++time) {
-      vertexes.clear();
       positions.clear();
       for (int i = 0; i < MAX_KV; ++i) X[i] = V;
       for (int i = 0; i < V; ++i) vertexes.emplace_back(i);
-      shuffle(vertexes.begin(), vertexes.end(), engine);
       for (int i = 1; i <= KR; ++i) {
         for (int j = 1; j <= KR; ++j) {
           int p = i * ROW + j;
@@ -153,16 +151,14 @@ int main() {
             int t = W[v][u];
             if (t) {
               n.score += t;
-              n.value += t << 4;
+              n.value += t << 10;
             } else {
-              n.value--;
+              n.value -= 1 << 4;
             }
           }
         };
-        if (ok[p]) {
-          ok[p] = false;
-          positions.emplace_back(p);
-          for (int v : vertexes) {
+        for (int v : vertexes) {
+          if (ok[p]) {
             state[p][v].score = 0;
             state[p][v].value = 0;
             add(state[p][v], v, X[p - ROW - 1]);
@@ -173,47 +169,45 @@ int main() {
             add(state[p][v], v, X[p + ROW - 1]);
             add(state[p][v], v, X[p + ROW]);
             add(state[p][v], v, X[p + ROW + 1]);
-            if (value < state[p][v].value) {
-              value = state[p][v].value;
-              bestVertex[p] = &state[p][v];
-            }
-          }
-        } else {
-          for (int v : vertexes) {
+          } else {
             add(state[p][v], v, X[n]);
-            if (value < state[p][v].value) {
-              value = state[p][v].value;
-              bestVertex[p] = &state[p][v];
-            }
           }
+          int t = state[p][v].value + (get_random() & ((1 << 4) - 1));
+          if (value < t) {
+            value = t;
+            bestVertex[p] = &state[p][v];
+          }
+        }
+        if (ok[p]) {
+          ok[p] = false;
+          positions.emplace_back(p);
         }
       };
       update(-1, (KR / 2 + 1) * ROW + (KR / 2 + 1));
       for (int i = 0; i < V; ++i) {
         Node* n = bestVertex[positions[0]];
-        {
-          int value = INT_MIN;
-          for (int p : positions) {
-            if (bestVertex[p]->value < 0) {
-              int value = INT_MIN;
-              for (int v : vertexes) {
-                if (value < state[p][v].value) {
-                  value = state[p][v].value;
-                  bestVertex[p] = &state[p][v];
-                }
+        int value = INT_MIN;
+        for (int p : positions) {
+          if (bestVertex[p]->value < 0) {
+            int value = INT_MIN;
+            for (int v : vertexes) {
+              if (value < state[p][v].value) {
+                value = state[p][v].value;
+                bestVertex[p] = &state[p][v];
               }
             }
-            if (value < bestVertex[p]->value) {
-              value = bestVertex[p]->value;
-              n = bestVertex[p];
-            }
+          }
+          int t = bestVertex[p]->value + (get_random() & ((1 << 4) - 1));
+          if (value < t) {
+            value = t;
+            n = bestVertex[p];
           }
         }
         X[n->pos] = n->vertex;
         vertexes.erase(find(vertexes.begin(), vertexes.end(), n->vertex));
         positions.erase(find(positions.begin(), positions.end(), n->pos));
         score += n->score;
-        for (int p : positions) state[p][n->vertex].value = -100;
+        for (int p : positions) state[p][n->vertex].value = -1000000;
         update(n->pos, n->pos - ROW - 1);
         update(n->pos, n->pos - ROW);
         update(n->pos, n->pos - ROW + 1);
@@ -227,64 +221,6 @@ int main() {
         bestScore = score;
         memcpy(best, X, sizeof(X));
       }
-    }
-  }
-  {  // Simulated Annealing
-    constexpr int LOG_SIZE = 1 << 10;
-    double log_d[LOG_SIZE];
-    uint8_t log_[LOG_SIZE];
-    int r = min(KR, (int)(sqrt(V * 1.2) + 0.9));
-    int n = 0;
-    for (int i = 0; i < MAX_KV; ++i) X[i] = V;
-    for (int i = 1; i <= r && n < V; ++i) {
-      for (int j = 1; j <= r && n < V; ++j) {
-        X[i * ROW + j] = n++;
-      }
-    }
-    memset(P, 0, sizeof(P));
-    for (int i = 1; i <= r; ++i) {
-      for (int j = 1; j <= r; ++j) {
-        int p = i * ROW + j;
-        P[p] = value(p);
-      }
-    }
-    for (int i = 0; i < LOG_SIZE; ++i) {
-      log_d[i] = -5 * log((i + 0.5) / LOG_SIZE) / TIME_LIMIT;
-    }
-    while (true) {
-      double time = TIME_LIMIT - timer.getElapsed();
-      if (time < 0) break;
-      for (int i = 0; i < LOG_SIZE; ++i)
-        log_[i] = min(20.0, round(log_d[i] * time));
-      for (int t = 0; t < 0x10000; ++t) {
-        unsigned m = get_random();
-        int p1 = (((m >> 0) % r + 1) << 6) | ((m >> 6) % r + 1);
-        int p2 = (((m >> 12) % r + 1) << 6) | ((m >> 18) % r + 1);
-        if (X[p1] == X[p2]) continue;
-        int pv = P[p1] + P[p2];
-        swap(X[p1], X[p2]);
-        int v1 = value(p1), v2 = value(p2);
-        if ((pv - v1 - v2) > log_[get_random() & (LOG_SIZE - 1)]) {
-          swap(X[p1], X[p2]);
-        } else {
-          diff(p2, p1);
-          diff(p1, p2);
-          P[p1] = v1;
-          P[p2] = v2;
-        }
-      }
-    }
-    int score = 0;
-    for (int i = 1; i <= r; ++i) {
-      for (int j = 1; j <= r; ++j) {
-        score += value(i * ROW + j);
-      }
-    }
-    score /= 2;
-    // cerr << bestScore << " " << score << endl;
-    if (bestScore < score) {
-      bestScore = score;
-      memcpy(best, X, sizeof(X));
     }
   }
   {  // output
